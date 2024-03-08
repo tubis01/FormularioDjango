91dac3349466e52086d879d469574425762b878a
MynorBaranLeiva <157561512+MynorBaranLeiva@users.noreply.github.com>
2024-03-08 16:11:01 -0600

    aplicacion final funcionando

--- registroAlumnos/models.py
+++ registroAlumnos/models.py
@@ -1,12 +1,7 @@
 from django.db import models
-
-# Create your models here.
-
 from django.db import models
-# agragamos la libreria para poder calcular la edad 
 from datetime import date
 
-# definimos nuestro model de Alumno 'los campos que contendra'
 class Alumno(models.Model):
     carnet = models.CharField(max_length=15, unique=True)
     nombres = models.CharField(max_length=100)
@@ -17,7 +12,7 @@ class Alumno(models.Model):
     def __str__(self):
         return self.nombres + ' ' + self.apellidos
 
-# funcion para calcular la edad
+
 def calcular_edad(fecha_nacimiento):
     hoy = date.today()
     return hoy.year - fecha_nacimiento.year - ((hoy.month, hoy.day) < (fecha_nacimiento.month, fecha_nacimiento.day))
--- registroAlumnos/views.py
+++ registroAlumnos/views.py
@@ -1,32 +1,32 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Alumno
-from .formulario import AlumnoForm  # Asegúrate de haber creado AlumnoForm en forms.py
-from collections import Counter #para contar los alumnos que hay de cada edad
+from .forms import AlumnoForm  
+from django.contrib import messages
+from collections import Counter
 from datetime import date
 
-# Vista para listar alumnos
 def listarAlumno(request):
     alumnos = Alumno.objects.all()
     return render(request, 'registroAlumnos/listarAlumno.html', {'alumnos': alumnos})
 
-# Vista para crear alumno
 def crearAlumno(request):
     if request.method == 'POST':
         form = AlumnoForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Alumno agregado correctamente.')
             return redirect('listarAlumno')
     else:
         form = AlumnoForm()
     return render(request, 'registroAlumnos/crearAlumno.html', {'form': form})
 
-# Vista para modificar alumno
 def modificarAlumno(request, pk):
     alumno = get_object_or_404(Alumno, pk=pk)
     if request.method == "POST":
         form = AlumnoForm(request.POST, instance=alumno)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Alumno Actualizado correctamente.')
             return redirect('listarAlumno')
     else:
         form = AlumnoForm(instance=alumno)
@@ -37,18 +37,16 @@ def eliminarAlumno(request, pk):
     alumno = get_object_or_404(Alumno, pk=pk)
     if request.method == 'POST':
         alumno.delete()
+        messages.success(request, 'Alumno Elminado correctamente.')
         return redirect('listarAlumno')
     return render(request, 'registroAlumnos/eliminarAlumno.html', {'alumno': alumno})
 
-# vista para estadisticas de alumnos por edad
 def estadisticas_edad(request):
     alumnos = Alumno.objects.all()
     edades = [calcular_edad(alumno.fechaNacimiento) for alumno in alumnos]
     conteo_edades = Counter(edades)
     estadisticas = sorted(conteo_edades.items())
     return render(request, 'registroAlumnos/estadisticasEdad.html', {'estadisticas': estadisticas})
-
-
 
 def calcular_edad(fecha_nacimiento):
     hoy = date.today()
